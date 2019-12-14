9a4449f616bb9e708b35d224d0296bb1115c7e0f
Fujio Nakatsuka <com.nifty.cbm01799>
2019-12-14 17:57:21 +0900

    ラベル２配置

---
+++ TestApp/NextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NextViewController.swift
+//  TestApp
+//
+//  Created by 中塚富士雄 on 2019/12/14.
+//  Copyright © 2019 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    
+    var passText = String()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label2.text = passText
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
