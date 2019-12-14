4513c3399d9c45a98f682bbfca7e737dcf51e9a0
Yuta Fujii <yutayutakid21@gmail.com>
2019-12-14 16:59:38 +0900

    課題

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //Storyboard上にNextViewControllerを作成し、
+    //NextViewControllerの上に好きなラベルを設置し、IBOutletでつなぎ、
+    //viewDidLoad内で、好きな文字をラベルのテキストとして書いてください。
+    //いくつ書いてもかまいません。
+    //また、label2を作成し、
+    //変数passTextをString型で宣言してください。
+    //最後にlabel2.text = passTextと書いてください。
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
